0171d4ce1be94a49a114d8ef35b42259412c6d09
groth001 <groth@unomaha.edu>
2020-12-01 10:43:33 -0800

    Chapter 7 Complete

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZ3JvdGgwMDEiLCJhIjoiY2tpNjZpc2pzMmh6bjJ4cXFobXh6dWh2cSJ9.TPxNGoxFjpiVRbFYREuZuQ';
+
   return ENV;
 };
